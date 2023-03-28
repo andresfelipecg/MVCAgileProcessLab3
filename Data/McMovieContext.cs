88abc5729b17bf7ca733e66a48ba5627603be425
Andres Cuero <andresfelipecuero@gmail.com>
2023-03-28 17:54:43 -0400

    update 4

---
+++ Data/McMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace McMovie.Data
+{
+    public class McMovieContext : DbContext
+    {
+        public McMovieContext(DbContextOptions<McMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
